d4b232d9421d69155389123e6c5ba88592796c6d
Xtrife <Alucard.557@gmail.com>
2025-02-24 03:02:45 -0600

    add initial level map

--- core/src/main/java/com/xtrife/mariobros/screens/PlayScreen.java
+++ core/src/main/java/com/xtrife/mariobros/screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -22,11 +25,25 @@ public class PlayScreen implements Screen {
   private Viewport viewport;
   private Hud hud;
 
+  private TmxMapLoader mapLoader;
+  private TiledMap map;
+  private OrthogonalTiledMapRenderer renderer;
+
   public PlayScreen(Main game) {
     this.game = game;
+    // create camera that will eventually follow Mario
     gamecam = new OrthographicCamera();
+
+    // maintain aspect ratio
     viewport = new FitViewport(Main.V_WIDTH, Main.V_HEIGHT, gamecam);
+    // create HUD
     hud = new Hud(game.batch);
+
+    // level map
+    mapLoader = new TmxMapLoader();
+    map = mapLoader.load("level0.tmx");
+    renderer = new OrthogonalTiledMapRenderer(map);
+    gamecam.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
   }
 
   @Override
@@ -36,8 +53,10 @@ public class PlayScreen implements Screen {
 
   @Override
   public void render(float delta) {
+    update(delta);
     Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    renderer.render();
 
     game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
     hud.stage.draw();
@@ -65,6 +84,19 @@ public class PlayScreen implements Screen {
 
   @Override
   public void dispose() {
+
+  }
+
+  public void update(float delta) {
+    handleInput(delta);
+    gamecam.update(); // always update cam
+    renderer.setView(gamecam); // render only what cam can see
+  }
+
+  private void handleInput(float delta) {
+    if (Gdx.input.isTouched()) {
+      gamecam.position.x += 100 * delta;
+    }
 
   }
 }
